68bf7cafba2cc4adeee50a17f06fad4d706fd915
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-05-21 11:54:51 -0600

    Add: maximum-sum-circular-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-3.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun maxSubarraySumCircular(nums: IntArray): Int {
+        var max = Int.MIN_VALUE
+        var min = Int.MAX_VALUE
+        var currentMax = 0
+        var currentMin = 0
+        var total = 0
+        for (num in nums) {
+            total += num
+            currentMax += num
+            currentMin += num
+            max = maxOf(max, currentMax)
+            min = minOf(min, currentMin)
+            currentMax = maxOf(0, currentMax)
+            currentMin = minOf(0, currentMin)
+        }
+        if (total == min) {
+            return max
+        }
+        return maxOf(max, total - min)
+    }
+}
