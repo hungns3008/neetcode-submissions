1d841cae31a4bd90e996fcf8b836f08b341c4cfa
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-05-28 22:22:59 -0600

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        var prefix = IntArray(nums.count()) { 1 }
+
+        var current = nums[0]
+        for (i in 1 until nums.count()) {
+            prefix[i] = current
+            current *= nums[i]
+        }
+        current = nums[nums.count() - 1]
+        for (i in nums.count() - 2 downTo 0) {
+            prefix[i] = prefix[i] * current
+            current *= nums[i]
+        }
+        return prefix
+    }
+}
