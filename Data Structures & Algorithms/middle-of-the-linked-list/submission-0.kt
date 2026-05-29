9ea2c21e36696213e3eddc670cf488b354851348
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-05-29 11:10:05 -0600

    Add: middle-of-the-linked-list - submission-0

---
+++ Data Structures & Algorithms/middle-of-the-linked-list/submission-0.kt
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun middleNode(head: ListNode?): ListNode? {
+        var fast = head
+        var slow = head
+        while (fast != null && fast?.next != null) {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        return slow
+    }
+}
