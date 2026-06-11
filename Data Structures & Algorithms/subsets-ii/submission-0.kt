b96e58269057d5257083498caceef7e8c3cff916
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-06-11 10:42:28 -0600

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun subsetsWithDup(nums: IntArray): List<List<Int>> {
+        nums.sort()
+        val res = mutableListOf<List<Int>>()
+        var temp = mutableListOf<Int>()
+
+        fun backtrack(i: Int) {
+            if (i == nums.size) {
+                res.add(temp.toList())
+                return
+            }
+
+            temp.add(nums[i])
+            backtrack(i + 1)
+            
+            temp.removeLast()
+            
+            var j = i
+            while (j + 1 < nums.count() && nums[j] == nums[j + 1]) {
+                j += 1
+            }
+            backtrack(j + 1)
+
+        }
+
+        backtrack(0)
+        return res.toList()
+    }
+}
