5e1233bf2a391b3ca52f2a7b017add8c92c1b2b3
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-07-29 21:18:03 -0600

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.lastIndex
+        while (left <= right) {
+            var mid = (right + left) / 2
+            if (nums[mid] == target) {
+                return mid
+            }
+            if (nums[mid] <= nums[right]) {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            } else {
+                if (target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            }
+        }
+        return -1
+    }
+}
