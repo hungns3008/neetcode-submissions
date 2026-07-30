279b811468752da607fbd487b8dae9d9f0dfd21b
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-07-29 19:55:05 -0600

    Add: largest-rectangle-in-histogram - submission-2

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-2.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun largestRectangleArea(heights: IntArray): Int {
+        var queue = mutableListOf<IntArray>()
+        var output = 0
+        for (i in 0 until heights.count()) {
+            output = maxOf(output, heights[i])
+            val currentHeight = heights[i]
+            var currentIndex = i
+            while (queue.count() > 0 && queue.last()[1] > currentHeight) {
+                val last = queue.removeLast()
+                val size = i - last[0]
+                val square = size * last[1]
+                output = maxOf(output, square)
+                currentIndex = last[0]
+            }
+            queue.add(intArrayOf(currentIndex, currentHeight))
+
+        }
+        for (i in 0 until queue.count()) {
+            output = maxOf(output, (heights.count() - queue[i][0]) * queue[i][1])
+        }
+        return output
+    }
+}
