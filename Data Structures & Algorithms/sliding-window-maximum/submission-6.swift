14b5e22be4a46cb41dd2df8b5b73d8b94409ae5d
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-08-28 17:44:40 -0600

    Add: sliding-window-maximum - submission-6

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-6.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
+        var dequeue = [Int]()
+        var left = 0
+        var right = 0
+        var output = [Int]()
+        while right < nums.count {
+            while dequeue.count > 0 && dequeue.last! < nums[right] {
+                dequeue.removeLast()
+            }
+            dequeue.append(nums[right])
+            if right - left + 1 == k {
+                output.append(dequeue[0])
+                if nums[left] == dequeue[0] {
+                    dequeue.removeFirst()
+                }
+                left += 1
+            }
+            right += 1
+        }
+        return output
+    }
+}
