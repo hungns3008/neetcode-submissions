dc62b9b60b8bf83d4b7c3e9d9acd2863642c609e
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-08-28 20:03:48 -0600

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.kt
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun reorderList(head: ListNode?): Unit {
+
+        fun reverse(node: ListNode?): ListNode? {
+            var prev: ListNode? = null
+            var current = node
+            while (current != null) {
+                var next = current?.next
+                current?.next = prev
+                prev = current
+                current = next
+            }
+            return prev
+        }
+
+        var slow = head
+        var fast = head
+        while (fast != null && fast?.next != null) {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+
+        var leftNode = head
+        var rightNode = reverse(slow)
+
+        while (leftNode != rightNode) {
+            var l1 = leftNode?.next
+            var l2 = rightNode?.next
+            leftNode?.next = rightNode
+            rightNode?.next = l1
+
+            leftNode = l1
+            rightNode = l2
+        }
+        
+    }
+}
