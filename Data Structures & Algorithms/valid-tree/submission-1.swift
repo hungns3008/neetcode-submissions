74e4261cb55abe62ea325ed4e1e4b9e28cb4172c
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-08-29 16:43:07 -0600

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.swift
@@ -0,0 +1,46 @@
+class Solution {
+    func validTree(_ n: Int, _ edges: [[Int]]) -> Bool {
+
+        var rank = Array(repeating: 0, count: n)
+        var par = Array(repeating: 0, count: n)
+        
+        for i in 0 ..< n {
+            par[i] = i
+        }
+
+        func find(_ n: Int) -> Int {
+            if n != par[n] {
+                par[n] = find(par[n])
+            }
+            return par[n]
+        }
+
+        func union(_ n1: Int, _ n2: Int) -> Bool {
+            var p1 = find(n1)
+            var p2 = find(n2)
+            if p1 == p2 {
+                return false
+            }
+            if rank[p1] >= rank[p2] {
+                par[p2] = p1
+                rank[p1] += 1
+            } else {
+                par[p1] = p2
+                rank[p2] += 1 
+            }
+            return true
+        }
+
+        if edges.count != n - 1 {
+            return false
+        }
+
+        for edge in edges {
+            if !union(edge[0], edge[1]) {
+                return false
+            }
+        }
+        return true
+
+    }
+}
