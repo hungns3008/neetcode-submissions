745f5aced1fb69f10c7946b5232492f58f8a6249
hungns3008 <54855197+hungns3008@users.noreply.github.com>
2026-08-30 08:24:42 -0600

    Add: hand-of-straights - submission-3

---
+++ Data Structures & Algorithms/hand-of-straights/submission-3.swift
@@ -0,0 +1,32 @@
+class Solution {
+    func isNStraightHand(_ hand: [Int], _ groupSize: Int) -> Bool {
+        if hand.count % groupSize != 0 {
+            return false
+        }
+
+        var count = [Int: Int]()
+        for num in hand {
+            if count[num] == nil {
+                count[num] = 0
+            }
+            count[num] = count[num]! + 1
+        }
+
+        for num in hand {
+            var start = num
+            while (count[start - 1] ?? 0) > 0 {
+                start -= 1
+            }
+            while (count[start] ?? 0) > 0 {
+                for i in start..<(start + groupSize) {
+                    if (count[i] ?? 0) == 0 {
+                        return false
+                    }
+                    count[i]! -= 1
+                }
+            }
+        }
+
+        return true
+    }
+}
